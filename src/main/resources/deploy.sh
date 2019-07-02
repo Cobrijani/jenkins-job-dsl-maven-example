a607fdd0a589c1a7fd9f1a5f2b7591c7128adb68
Cobrijani <stefanbraticns@gmail.com>
2019-07-02 22:30:48 +0100

    fixed deploy script

--- src/main/resources/deploy.sh
+++ src/main/resources/deploy.sh
@@ -7,7 +7,7 @@ if [ -f "$FILE" ]; then
     echo "$FILE exists, performing deployment"
     docker-compose -f $FILE stop
     docker-compose -f $FILE rm -f 
-    docker-compose up -d --force-recreate
+    docker-compose -f $FILE up -d --force-recreate
     echo "Complete finished"
 else
     echo "Deployment yaml not found"
